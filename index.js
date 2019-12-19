767286735dc5e6c2044770f4f2a0638113325212
Max Forssén <max.forssen@mentimeter.com>
2019-12-19 11:01:41 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const socket = require('socket.io');
+
+//app setup
+app = express();
+const server = app.listen(4000,function(){
+    console.log('Listening to port 4000');
+});
+
+//static file
+app.use(express.static('public'));
+
+//socket setup
+const io = socket(server);
+
+io.on('connection',function(socket){
+    console.log('made socket connection',socket.id);
+
+    //handle chat events
+    socket.on('chat',function(data){
+        io.sockets.emit('chat',data)
+    });
+
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing',data)
+    });
+
+});
+
